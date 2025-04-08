4e3ad50ff80ec13afde3cb34dd837f61a720f233
Mason Kubiak <mayakubiak10@gmail.com>
2025-04-08 11:40:29 -0400

    Added Controller for Title

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+const uuid = Uuid();
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
+};
+final formatter = DateFormat.yMd();
+
+enum Category { food, travel, leisure, work }
+
 class Expense {
-  const Expense(
-      {required this.title, required this.amount, required this.date});
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,55 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => Text('My Modal'),
+    );
+  }
+
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Cheeseburger',
+        amount: 11.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Movie Ticket',
+        amount: 18.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Text("Chart"),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
